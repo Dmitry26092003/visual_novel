deb6defbfe003f8eb44a7a9e0ebab5f9c7c8f5dd
Dmitry26092003 <Bal.Dima2003@yandex.ru>
2019-02-11 11:10:54 +0300

    пофикшен баг с настройками

--- menu.py
+++ menu.py
@@ -5,6 +5,7 @@ from ctypes  import *
 import time
 audio_fl = bool(int(open('data\settings.txt').read().split('\n')[0].split(' = ')[1]))
 music_fl = bool(int(open('data\settings.txt').read().split('\n')[1].split(' = ')[1]))
+size = 'HD'
 print(audio_fl, music_fl)
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
@@ -25,7 +26,7 @@ def settings():
     global music_fl
     global menu_music
     fl = True
-    menu = load_image("menu\settings\main.png")
+    menu = load_image("menu\{}\settings\main.png".format(size))
     screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
     while fl:
         for event in pygame.event.get():
@@ -37,7 +38,6 @@ def settings():
                     if audio_fl:
                         click_sound.play()                    
                     fl = False
-                    menu = load_image("menu\start_menu\main_0.png")
                 if 340 < xx < 401:
                     if 210 < yy < 238:
                         if audio_fl:
@@ -59,17 +59,17 @@ def settings():
                 xx = xx - (x-menu.get_width())//2
                 yy = yy - (y-menu.get_height())//2
                 if 25 < xx < 280 and 340 < yy < 415:
-                    menu = load_image("menu\settings\main_exit.png")
+                    menu = load_image("menu\{}\settings\main_exit.png".format(size))
                 else:
-                    menu = load_image("menu\settings\main.png")
+                    menu = load_image("menu\{}\settings\main.png".format(size))
         if audio_fl:
-            audio_bt = load_image("menu\settings\on.png")
+            audio_bt = load_image("menu\{}\settings\on.png".format(size))
         else:
-            audio_bt = load_image("menu\settings\off.png")
+            audio_bt = load_image("menu\{}\settings\off.png".format(size))
         if music_fl:
-            music_bt = load_image("menu\settings\on.png")
+            music_bt = load_image("menu\{}\settings\on.png".format(size))
         else:
-            music_bt = load_image("menu\settings\off.png")
+            music_bt = load_image("menu\{}\settings\off.png".format(size))
         screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
         screen.blit(audio_bt, ((x-menu.get_width())//2 + 340, (y-menu.get_height())//2 + 210))
         screen.blit(music_bt, ((x-menu.get_width())//2 + 340, (y-menu.get_height())//2 + 280))
@@ -94,7 +94,7 @@ click_sound = pygame.mixer.Sound(os.path.join('data\music\click.wav'))
 screen.fill((0, 0, 0))
 pygame.display.flip()
 # рисование меню
-menu = load_image("menu\start_menu\main_0.png")
+menu = load_image("menu\{}\start_menu\main_0.png".format(size))
 screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
 pygame.display.flip()
 
@@ -102,6 +102,7 @@ pygame.display.flip()
 # Игровой цикл
 running = True
 while running:
+    pygame.display.flip()    
     # обработка событий
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -120,6 +121,9 @@ while running:
                     if audio_fl:
                         click_sound.play()                        
                     settings()
+                    menu = load_image("menu\{}\start_menu\main_0.png".format(size))
+                    screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))                    
+                    pygame.display.flip()                    
                 elif 170 < yy < 245:
                     if audio_fl:
                         click_sound.play()                        
@@ -137,31 +141,31 @@ while running:
             yy = yy - (y-menu.get_height())//2
             if 25 < xx < 425:
                 if 170 < yy < 245:
-                    menu = load_image("menu\start_menu\main_load_game.png")
+                    menu = load_image("menu\{}\start_menu\main_load_game.png".format(size))
                     screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
                     pygame.display.flip()
                 elif 255 < yy < 330:
-                    menu = load_image("menu\start_menu\main_new_game.png")
+                    menu = load_image("menu\{}\start_menu\main_new_game.png".format(size))
                     screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
                     pygame.display.flip()
                 elif 350 < yy < 430:
-                    menu = load_image("menu\start_menu\main_settings.png")
+                    menu = load_image("menu\{}\start_menu\main_settings.png".format(size))
                     screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
                     pygame.display.flip()
                 elif 440 < yy < 515:
-                    menu = load_image("menu\start_menu\main_info.png")
+                    menu = load_image("menu\{}\start_menu\main_info.png".format(size))
                     screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
                     pygame.display.flip()
                 elif 520 < yy < 595:
-                    menu = load_image("menu\start_menu\main_exit.png")
+                    menu = load_image("menu\{}\start_menu\main_exit.png".format(size))
                     screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
                     pygame.display.flip()
                 else:
-                    menu = load_image("menu\start_menu\main_0.png")
+                    menu = load_image("menu\{}\start_menu\main_0.png".format(size))
                     screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
                     pygame.display.flip()
             else:
-                menu = load_image("menu\start_menu\main_0.png")
+                menu = load_image("menu\{}\start_menu\main_0.png".format(size))
                 screen.blit(menu, ((x-menu.get_width())//2, (y-menu.get_height())//2))
                 pygame.display.flip()                
     # обновление экрана
